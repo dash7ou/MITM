6dca07077091c28accb579420ef633f193b45182
dashzou <fxfddfno@gmail.com>
2020-07-03 04:23:34 -0400

    convare the packet to scapy packet to get more controll

--- net_cut.py
+++ net_cut.py
@@ -1,15 +1,22 @@
 # !/usr/bin/env python
 
 import netfilterqueue
+import scapy.all as scapy
 
 
 def process_packet(packet):
-    print(packet)
     '''
-       packet.accept() accept the packet and connection throw
-	packet.drop() cut the internet connection :)
+        packet.accept() accept the packet and connection throw
+        packet.drop() cut the internet connection :)
+        packet.get_payload() show all info about packet like scapy
+        but we need scapy to add packet to it because i need to deal with this packet as snifft
     '''
-    packet.drop()
+
+    # here we add packet to IP layer in scapy
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
+    packet.accept()
+
 
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
