638b533ea5d5485286d1a956a32dc635330d77f3
dashzou <fxfddfno@gmail.com>
2020-07-17 04:16:33 -0400

    capturing & anlising arp response

---
+++ arpspoof_detector.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+
+import scapy.all as scapy
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+
+
+sniff("eth0")
