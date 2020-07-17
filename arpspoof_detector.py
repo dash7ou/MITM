d8aa6182ecc30fdf49d794f8ca861ffd497d1404
dashzou <fxfddfno@gmail.com>
2020-07-17 04:35:48 -0400

    if under attack show worning

--- arpspoof_detector.py
+++ arpspoof_detector.py
@@ -3,12 +3,35 @@
 import scapy.all as scapy
 
 
+def get_mac(ip):
+    # send packet
+    arp_request = scapy.ARP(pdst=ip)
+    # create broadcast
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_boradcast = broadcast/arp_request
+
+    # srp send and get response to get mac_target
+    answared = scapy.srp(arp_request_boradcast, timeout=10, verbose=False)[0]
+    try:
+        return answared[0][1].hwsrc
+    except:
+        get_mac(ip)
+
+
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
 def process_sniffed_packet(packet):
-    if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+    try:
+        if packet.haslayer(scapy.ARP) and packet[scapy.ARP].op == 2:
+            real_mac = get_mac(packet[scapy.ARP].psrc)
+            response_mac = packet[scapy.ARP].hwsrc
+
+            if real_mac != response_mac:
+                print('[+] You are under attack')
+    except IndexError:
+        pass
 
 
 sniff("eth0")
